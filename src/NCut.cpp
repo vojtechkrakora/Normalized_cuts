34337e9772e17a3d1d2acd4d5ff2d26908ea3f8d
SpeedEX505 <speedex505@gmail.com>
2014-11-26 14:30:02 +0100

    kostra programu

--- src/NCut.cpp
+++ src/NCut.cpp
@@ -1,1 +1,33 @@
 #include "NCut.h"
+
+// vytvori matici podobnosti
+void NCut::CreateAffinityMatrix(){
+
+}
+	
+// vytvori degree matici
+void NCut::CreateDegreeMatrix(){
+}
+// vypocita vlastni cislo (2nd)
+void NCut::ComputeEigenValue(){
+
+}
+// vypocita vlastni vektor
+void NCut::ComputeEigenVector(){
+
+}
+//provede rez
+void NCut::NCut(){
+
+}
+
+NCut::NCut(double *** input,int lenght1, int lenght2, int lenght3){
+
+}
+void NCut::Sementation(){
+
+}
+int** NCut::getResult(){
+
+return null;
+}
--- src/NCut.h
+++ src/NCut.h
@@ -2,17 +2,22 @@
 #define NCUT_H
 class NCut{
 private:
-	double *** matrix;
-	double *** affinityMatrix;
-	double *** degreeMatrix;
-	int *** resultMatrix;
+	double ** adjacencyMatrix;		//matice sousednosti
+	double ** affinityMatrix;		//matice podobnosti
+	double ** degreeMatrix;			//matice degree
+	int *** resultMatrix;			//vysledna segmentace
+	double eigenvalue;				//vlastni cislo 2nd
+	double * eigenvector;			//vlastni vektor
 
-	void CreateGraph();
-	void SolveEquation();
-	double * getEigenVector
+	void CreateAffinityMatrix();	// vytvori matici podobnosti
+	void CreateDegreeMatrix(); 		// vytvori degree matici
+	void ComputeEigenValue();		// vypocita vlastni cislo (2nd)
+	void ComputeEigenVector();		// vypocita vlastni vektor
+	void NCut()						// provede rez
 	
 public:
-	NCut(double *** input);
-	void Sementation();	
+	NCut(double *** input,int lenght1, int lenght2, int lenght3); // vyplni inputMatrix ("splacne")
+	void Sementation();				// vlastni segmentace
+	int ** getResult();				// vrati vysledek
 };
 #endif
